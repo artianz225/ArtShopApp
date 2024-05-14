2e7896e83028d479cc8231767f6890938dcc8ae0
Arthur John Philipps Epiz <artianz225@gmail.com>
2024-05-14 15:41:39 +0800

    testing vite config configuration

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/ArtShopApp': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/ArtShopApp/'
 });
