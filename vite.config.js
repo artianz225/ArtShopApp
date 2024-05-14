afb90871607b86a61c7b20fa34efb44dcc40c49d
Arthur John Philipps Epiz <artianz225@gmail.com>
2024-05-14 15:36:22 +0800

    setting config proxy

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5173',
+      '/ArtShopApp': {
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
